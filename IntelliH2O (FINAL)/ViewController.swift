42460c414caad53d523f3600cb4ccd3302d3f894
legozebra <legozebra@gmail.com>
2015-04-08 14:08:54 -0700

    Initial Commit

---
+++ IntelliH2O (FINAL)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IntelliH2O (FINAL)
+//
+//  Created by Lego Zebra on 4/8/15.
+//  Copyright (c) 2015 IntelliNinja Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
